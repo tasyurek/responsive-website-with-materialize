5bbb988f549f48dba4605fbd9bd798b5faa7c76f
onurcantasyurek <onurcantasyurek@gmail.com>
2018-12-17 00:11:41 +0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".sidenav").sidenav();
+  $(".materialboxed").materialbox();
+  $(".parallax").parallax();
+  $(".tabs").tabs();
+  $(".datepicker").datepicker({});
+  $(".scrollspy").scrollSpy();
+  $(".fixed-action-btn").floatingActionButton();
+  $(".tooltipped").tooltip();
+});
